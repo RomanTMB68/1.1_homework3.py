5276024aef6a95841ec41bb436842199b62cfee5
RomanTMB68 <medoedtmb@gmail.com>
2024-06-11 18:05:03 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,16 @@
+a=input("name:")
+
+b=input("age:")
+
+year=2024
+date_of_berth=input('berthday')
+
+New_age=year-int(date_of_berth)
+
+
+is_student=True
+
+print("Name:",a)
+print("Age:",b)
+print("New_age:", New_age)
+print("is_student:", is_student)
